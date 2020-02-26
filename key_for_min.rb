5d3c433c48dbb2eb88843ed0ac1e634483fef2c3
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-26 14:14:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  i = 0
+    
+    
+  end
+    key
 end
+
+#organize value, which is least 
+#return key associated with the lowest value but dont #return value
+#an_array[i], an_array[i+1] = an_array[i+1], an_array[i]
